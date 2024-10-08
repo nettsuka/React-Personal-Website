457ab7244c2563efb650d6dad17239dd7a2715ec
Nettsuka <jonathan.chhou@outlook.com>
2024-10-08 09:21:11 -0700

    added draggable container #app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
+import { useEffect, useRef } from "react";
 import './App.css';
 
 function App() {
+
+  const isClicked = useRef(false);
+  const coord = useRef({startX: 0, startY: 0, lastX:0, lastY:0});
+
+  const containerRef = useRef(null);
+  const windowRef = useRef(null);
+
+  useEffect(() =>{
+    if(!windowRef.current || !containerRef.current) return
+    const window = windowRef.current;
+    const container = containerRef.current;
+
+    const onMouseDown = (e) => {
+      isClicked.current = true;
+      coord.current.startX = e.clientX;
+      coord.current.startY = e.clientY;
+    }
+
+    const onMouseUp = (e) => {
+      isClicked.current = false;
+      coord.current.lastX = window.offsetLeft;
+      coord.current.lastY = window.offsetTop;
+    }
+
+    const onMouseMove = (e) => {
+      if(!isClicked.current) return;
+
+      const nextX = e.clientX - coord.current.startX + coord.current.lastX;
+      const nextY = e.clientY - coord.current.startY + coord.current.lastY;
+
+      window.style.top = `${nextY}px`;
+      window.style.left = `${nextX}px`;
+    }
+
+    window.addEventListener('mousedown',onMouseDown);
+    window.addEventListener('mouseup',onMouseUp);
+    container.addEventListener('mousemove', onMouseMove);
+    container.addEventListener('mouseleave', onMouseUp);
+
+    const cleanup = () => {
+      window.removeEventListener('mousedown', onMouseDown);
+      window.removeEventListener('mouseup', onMouseUp);
+      container.removeEventListener('mousemove', onMouseMove);
+      container.addEventListener('mouseleave', onMouseUp);
+    }
+
+    return cleanup;
+  })
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div ref={containerRef} className='container'>
+        <div ref={windowRef} className='window'>There is text in here.</div>
+      </div>
+    </main>
   );
 }
 
